8e8f544135bf36ef7b8dec3e50eb85c1ac457e6a
jay shah <shahjay00785@gmail.com>
2018-07-23 20:44:36 +0530

    Update withoutcallback.js

---
+++ withoutcallback.js
@@ -0,0 +1,12 @@
+let calc =function(num1,num2,calcType){
+
+    if((calcType)=="add"){
+        return num1+num2;
+    }
+    else if(calcType=="mul"){
+        return num1*numb2;
+    }
+
+
+};
+calc(2,3,"add");
